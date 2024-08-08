1a3e2c8f444cc8ac79a4240d898d85a6e2e2f971
Rustam Singh <rustamsingh743@gmail.com>
2024-08-08 22:28:22 +0530

    Used git stash command
    Signed-off-by: Rustam Singh <rustamsingh743@gmail.com>

---
+++ rus.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+
+	printf("This is a new apk I started working on.\n");
+	printf("An urgent work came so I need to drop the dev for this and start working on that.\n");
+	printf("So the soln??? -->    STASH it using git stash\n");
+
+
+	return 0;
+}
